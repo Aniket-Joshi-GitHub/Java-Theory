1f40cfb55781a40ee1b5070c1d469acf5e8d5bf1
Aniket Joshi <aniket5923858@gmail.com>
2024-04-27 22:13:27 +0530

    Pushed this update as earlier push removed the externally added folder

---
+++ Skeleton/Example1/Program1.java
@@ -0,0 +1,12 @@
+// Skeleton Program.
+// A method has been called from different class by below defined syntax.
+
+public class Program1
+{
+    public static void main(String[]args) {
+        {
+            System.out.println("Hello World");
+            Program2.sayHi();
+        }
+    }
+}
---
+++ Skeleton/Example3/Program6.java
@@ -0,0 +1,8 @@
+
+public class Program6
+{
+    public static void main(String[]args)
+    {
+      
+    }
+}
