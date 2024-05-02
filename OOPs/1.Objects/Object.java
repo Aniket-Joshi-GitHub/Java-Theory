6d66f44af41b318d4d12b08a7108f1b3db9b7ccb
Aniket Joshi <118168036+Aniket-Joshi-GitHub@users.noreply.github.com>
2024-05-02 16:31:22 +0530

    Update Object.java

--- OOPs/1.Objects/Object.java
+++ OOPs/1.Objects/Object.java
@@ -1,11 +1,12 @@
 public class Object 
 {
-    
+    //Below are the attributes of the objects that can be created through this class.
     String name;
     String Color;
     int number;
 
 
+    //Below is the function which an object of this class can perform.
     public void function1()
     {
         System.out.println("I am a function ");
