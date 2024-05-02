f88afeb80aefdc9437aecdbd9385f598760dcf2f
Aniket Joshi <118168036+Aniket-Joshi-GitHub@users.noreply.github.com>
2024-05-02 17:04:53 +0530

    Update Main.java

--- OOPs/3.Spam Variable/Main.java
+++ OOPs/3.Spam Variable/Main.java
@@ -1,6 +1,6 @@
  
-    /*Below is the external external variable which does not belong to Object class.
-     * This is not right as not object properties can modifed with external variable.
+    /*Below the first way to modify variable properties works but this not safe.
+     * We will make the attributes private so to make this process stop.
      * Hence we will introduce (Private variables) and getters and setters to make them inaccssible externally
      * Getters and setters are methods which are designed to modify the properties of Ojbects
      */
@@ -10,9 +10,9 @@ public class Main
   public static void main(String[]args)
   {
 
-    String name = "Spam Object";
 
-    Object obj1 = new Object(name, "Red", 10);
+    Object obj1 = new Object("Object1", "Red", 10);
+    obj1.name = "Spam Object";
     obj1.function1();
 
     Object obj2 = new Object("Object2", "Oranage", 15);
@@ -20,4 +20,4 @@ public class Main
 
     
   }
-}
+}}
