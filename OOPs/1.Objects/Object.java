be8d3fffbb492a7d133fabf9044ab5f64c1c985e
Aniket Joshi <118168036+Aniket-Joshi-GitHub@users.noreply.github.com>
2024-05-02 16:29:26 +0530

    Modification1

---
+++ OOPs/1.Objects/Object.java
@@ -0,0 +1,14 @@
+public class Object 
+{
+    
+    String name;
+    String Color;
+    int number;
+
+
+    public void function1()
+    {
+        System.out.println("I am a function ");
+    }
+
+}
