520c918f3a5bfa68c828f9551fa0593d05c18435
Aniket Joshi <118168036+Aniket-Joshi-GitHub@users.noreply.github.com>
2024-05-02 16:27:04 +0530

    Object Oriented Programming

---
+++ OOPs/Constructor/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+  public static void main(String[]args)
+  {
+    Object obj1 = new Object("Object1", "Red", 10);
+    obj1.function1();
+
+    Object obj2 = new Object("Object2", "Oranage", 15);
+    obj2.function1();
+
+    
+  }
+}
